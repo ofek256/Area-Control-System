15f4a27b69ccc0c4934c77177e3cacae2a49d0b4
ofek256 <ofek256a@gmail.com>
2022-12-05 16:03:10 +0200

    baba

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <RCWL_Tal.h>
 
 
-<<<<<<< HEAD
 /*
 long lastMsg = 0;
 char msg[50];
@@ -12,13 +11,11 @@ float humidity = 0;
 const int ledPin = 4;
 
 
-=======
 
 /* example setup
->>>>>>> 3486d9d5ed6455d1549540c015e7c628c73cc286
 void setup() {
   init();
-  
+
 }
 
 void loop() {
