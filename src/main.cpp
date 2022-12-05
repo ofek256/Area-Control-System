625d9c1977905d63e71a4375fd8e6d73bf504d63
ofek256 <ofek256a@gmail.com>
2022-12-05 16:20:50 +0200

    fixed some stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,6 @@
 //#include <Ultrasonic_Yasmin.h>
 
 
-/*
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-float temperature = 0;
-float humidity = 0;
-const int ledPin = 4;
-
-
 
 /* example setup
 void setup() {
