2262b4f3bc8743ace181cd5fcb60521aa1937743
talnadler10@gmail.com <talnadler10@gmail.com>
2022-12-05 13:04:55 +0700

    tal shite oooo

---
+++ src/RCWL.cpp
@@ -0,0 +1,36 @@
+#include <utils.cpp> // load the utilities file which includes useful functions we wrote
+
+int Sensor = 18;  
+int LED = 19;
+
+
+
+void setup() {
+  init();
+  pinMode (Sensor, INPUT); 
+  pinMode (LED, OUTPUT);   
+
+}
+
+void loop() {
+  cnctLoop();
+  int val = digitalRead(Sensor); //Read Pin as input
+     if (val > 0)
+     { 
+        digitalWrite(LED, HIGH);
+        Serial.println("Motion Detected");
+        client.publish("esp32/RCWLsensor", "Motion Detected");
+        delay(1000);
+       
+     }
+     if(val == 0)
+     {
+        digitalWrite(LED, LOW);
+        Serial.println("No Motion"); 
+        client.publish("esp32/RCWLsensor", "No Motion");
+        delay(1000);
+    
+     }
+
+
+}
