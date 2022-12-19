1b6ba8d7738e071bfb4277c2f56aff39ca0fe6d1
Tal <talnadler10@gmail.com>
2022-12-19 12:04:12 +0700

    fuck you<3

--- src/RCWL_Tal.h
+++ src/RCWL_Tal.h
@@ -1,11 +1,12 @@
 #include <utils.h> // load the utilities file which includes useful functions we wrote
 
-int Sensor = 17;  
-int LED = 18;
-
+int Sensor = 18;  
+int LED = 19;
+bool state = true; // defines the last state of motion 
+bool oldstate = true; 
 
 void setup() {
-//  init();
+  init();
   pinMode (Sensor, INPUT); 
   pinMode (LED, OUTPUT);   
   Serial.begin(115200);
@@ -13,24 +14,36 @@ void setup() {
 }
 
 void loop() {
-//  cnctLoop();
-  int val = digitalRead(Sensor); //Read Pin as input
+  int val = digitalRead(Sensor); // Read Pin as input
      if (val > 0)
      { 
+      state = true; 
         digitalWrite(LED, HIGH);
         Serial.println("Motion Detected");
-//        client.publish("esp32/RCWLsensor", "Motion Detected");
         delay(1000);
        
      }
      if (val == 0)
      {
+      state = false; 
         digitalWrite(LED, LOW);
         Serial.println("No Motion"); 
-//        client.publish("esp32/RCWLsensor", "No Motion");
         delay(1000);
     
      }
 
-
+cnctLoop();
+     while(true)
+     {
+      if (state != oldstate) 
+      {
+       client.publish("esp32/RCWLsensor", "Motion Detected"); 
+       
+      }
+      else 
+      {
+        client.publish("esp32/RCWLsensor", "No Motion");
+        oldstate = state ; 
+      }
+   }
 }
