7b5198ce0a9c776a7a92ff78973221e2a8942705
Yasmin <Yasmin.nizan@gmail.com>
2022-12-19 14:18:15 +0200

    yello

---
+++ src/ldr_yasmin.h
@@ -0,0 +1,28 @@
+#include <utils.h>
+#define ldr_pin 19
+int ldrnow;
+const int dark = 200;
+int check;
+void setup()
+{
+    pinMode(ldr_pin, INPUT);
+    Serial.begin(115200);
+}
+void loop()
+{
+    ldrnow=analogRead(ldr_pin);
+    Serial.print("ldr value: ");
+    Serial.println  (ldrnow);
+    if (ldrnow>200)
+    {
+        Serial.println("Lights on. People in FRC/workshop");
+        check=1;
+    }
+    else
+    {
+        Serial.println("Lights off. Room is empty");
+        check=0;
+    }
+    Serial.print("check= ");
+    Serial.println  (check);
+}
