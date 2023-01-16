c11b7c7553ee02a98875128e81c2f996f4a656c9
Tal <talnadler10@gmail.com>
2023-01-16 15:50:43 +0700

    first discord bot commit

---
+++ BunkerBot/index.js
@@ -0,0 +1,31 @@
+
+
+const Discord = require ('discord.js');
+
+const prefix = '!';
+
+const client = new Discord.Client({
+
+    allowedMentions : {
+        parse: [`users` , `roles`],
+        replieUser: true,
+
+    },
+    intents: [
+
+        "GUILDS",
+        "GUILD_MESSAGES" , 
+        "GUILD_PRESENCES",
+        "GUILD_MEMBERS",
+        "GUILD_MESSAGE_REACTIONS",
+
+    ],
+});
+
+client.on("ready" , () => {
+    console.log("BunkerBot is online!")
+})
+
+
+client.login("MTA2NDQ5NTIzNDA4NTU3MjYxOA.GWJSYs.To_TeL5-CpHIZwmm5KoS9ST8psfzfeEwr2MqC4")
+
