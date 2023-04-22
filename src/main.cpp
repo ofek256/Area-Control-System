d013fc17b878c73adbe806b9679db91bc9805e3a
Yasmin <Yasmin.nizan@gmail.com>
2023-04-22 19:36:23 +0300

    main commenting

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,16 @@
 //#include <RCWL_Tal.h>
 //#include <Ultrasonic_Yasmin.h>
-<<<<<<< HEAD
-//#include <ldr_yasmin.h>
-#include <PIR-Tal.h>
-=======
+
 //#include <Ultrasonic_Yasmin.h>
 //#include <ldr_yasmin.h>
 //#include <PIR-Tal.h>
 //#include pms7003_yasmin.h
->>>>>>> dev
 //#include <pms_yasmin.h>
 #include <ldr_and_pms.h>
 //include <mqtt_test.h>
-<<<<<<< HEAD
-
-=======
 //#include <RCWL_without_MQTT.h>
 //#include <PIR_without_MQTT.h>
->>>>>>> dev
+
 /* //example setup
 void setup() {
   stpLoop();
